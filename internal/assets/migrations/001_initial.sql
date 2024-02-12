aa3319e7576ef7bfbf2549f28f02f177acce2bd5
evoaway <anatolii.mandzynets@gmail.com>
2024-02-12 11:26:54 +0200

    Add migrations

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -1,3 +1,13 @@
 -- +migrate Up
+create table if not exists transfers
+(
+    id bigserial primary key not null,
+    from_address char(42) not null,
+    to_address char(42) not null,
+    value numeric not null
+    );
+create index if not exists transfers_index on transfers (from_address, to_address);
 
 -- +migrate Down
+drop table if exists transfers;
+drop index if exists transfers_index;
