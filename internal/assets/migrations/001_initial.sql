36d112fb155a726d37ffa5202a130953c0c6f4ca
evoaway <anatolii.mandzynets@gmail.com>
2024-02-13 19:16:44 +0200

    Update value data type

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -4,7 +4,7 @@ create table if not exists transfers
     id bigserial primary key not null,
     from_address char(42) not null,
     to_address char(42) not null,
-    value numeric not null
+    value numeric(78, 0) not null
     );
 create index if not exists transfers_index on transfers (from_address, to_address);
 
