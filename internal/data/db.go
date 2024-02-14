94aabcf93fa24a642504875a34f972a38143ad41
evoaway <anatolii.mandzynets@gmail.com>
2024-02-14 15:37:22 +0200

    Add handler response

--- internal/data/db.go
+++ internal/data/db.go
@@ -30,7 +30,7 @@ func (d Database) AddTransfer(from, to string, value *big.Int) error {
 
 // struct will be moved
 type Transfer struct {
-	Id          int    `db:"id"`
+	Id          int64  `db:"id"`
 	FromAddress string `db:"from_address"`
 	ToAddress   string `db:"to_address"`
 	Value       string `db:"value"`
--- internal/service/handlers/get_transfers_by_address.go
+++ internal/service/handlers/get_transfers_by_address.go
@@ -1,21 +1,38 @@
 package handlers
 
 import (
+	"github.com/evoaway/erc20-transfers-storage-svc/internal/data"
 	"github.com/evoaway/erc20-transfers-storage-svc/internal/service/requests"
+	"github.com/evoaway/erc20-transfers-storage-svc/resources"
 	"gitlab.com/distributed_lab/ape"
 	"gitlab.com/distributed_lab/ape/problems"
-	"log"
 	"net/http"
+	"strconv"
 )
 
 func GetTransfersByAddress(w http.ResponseWriter, r *http.Request) {
 	address := requests.NewGetAddress(r)
 	err, trs := DB(r).SelectTransfersByAddress(address)
-	log.Println(trs)
 	if err != nil {
 		Log(r).WithError(err).Error("error processing get_transaction request")
 		ape.RenderErr(w, problems.InternalError())
 		return
 	}
-	w.Write([]byte(address))
+	ape.Render(w, resources.TransferListResponse{
+		Data: getArray(trs),
+	})
+}
+
+func getArray(transfers []data.Transfer) []resources.Transfer {
+	var resTransfers = make([]resources.Transfer, len(transfers))
+	for i, t := range transfers {
+		resTransfers[i].Key = resources.Key{ID: strconv.Itoa(i), Type: resources.TRANSFER}
+		resTransfers[i].Attributes = resources.TransferAttributes{
+			Id:    t.Id,
+			From:  t.FromAddress,
+			To:    t.ToAddress,
+			Value: t.Value,
+		}
+	}
+	return resTransfers
 }
